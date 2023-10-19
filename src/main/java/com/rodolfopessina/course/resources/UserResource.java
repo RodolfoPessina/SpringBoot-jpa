adde9c62bb8ce904de52a3c573dd8f7b232d8f62
Rodolfo Pessina <rodolfopessina@yahoo.com.br>
2023-10-19 00:05:00 -0300

    User entities

---
+++ src/main/java/com/rodolfopessina/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.rodolfopessina.course.resources;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodolfopessina.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1l, "maraia", "maria@gamil.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
