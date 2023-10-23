d3056b46e50eecddf194ac79fd1b9f03b24bfd9a
Rodolfo Pessina <rodolfopessina@yahoo.com.br>
2023-10-23 00:57:36 -0300

    subtotal

--- src/main/java/com/rodolfopessina/course/entities/Order.java
+++ src/main/java/com/rodolfopessina/course/entities/Order.java
@@ -43,7 +43,9 @@ public class Order implements Serializable {
 	private Set<OrderItem> items = new HashSet<>();
 	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	
 	private Payment payment;
+	
 	
 	public Order() {
 	}
@@ -104,6 +106,14 @@ public class Order implements Serializable {
 	
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+	
+	public double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
